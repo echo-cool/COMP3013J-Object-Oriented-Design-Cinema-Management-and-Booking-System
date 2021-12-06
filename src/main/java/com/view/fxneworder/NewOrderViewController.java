e660dbc5c4ccb3f57f3248750f8b3730144ce8f9
bug_creator <wenrui.liu@ucdconnect.ie>
2021-12-06 19:36:53 +0800

    'temp'

---
+++ src/main/java/com/view/fxneworder/NewOrderViewController.java
@@ -0,0 +1,11 @@
+package com.view.fxneworder;
+
+/**
+ * @Author: WangYuyang
+ * @Date: 2021/10/5-14:45
+ * @Project: comp3013j_assignment
+ * @Package: com.view.fxneworder
+ * @Description:
+ **/
+public class NewOrderViewController {
+}
