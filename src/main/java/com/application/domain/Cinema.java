b323abe14dd5973b901f2d40648833d405195a91
bug_creator <wenrui.liu@ucdconnect.ie>
2021-12-06 16:51:12 +0800

    temp

--- src/main/java/com/application/domain/Cinema.java
+++ src/main/java/com/application/domain/Cinema.java
@@ -4,6 +4,9 @@ package com.application.domain;
 import com.application.domain.mappers.MovieMapper;
 import com.application.domain.mappers.ScreenMapper;
 import com.application.domain.mappers.ScreeningMapper;
+import com.application.models.Movie;
+import com.application.models.Screen;
+import com.application.models.Screening;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -18,7 +21,27 @@ public class Cinema {
     ScreeningMapper screeningMapper;
 
     public void scheduleScreening(LocalDate date, LocalTime start_time,int screen_no,String movie_name){
-
+        Screen screen=screenMapper.getScreenForOid(screen_no);
+        Movie movie=movieMapper.getMovie(movie_name);
+        screeningMapper.scheduleScreening(date,start_time,screen,movie);
     }
+
+    public void updateScreening(Screening selected){
+        screeningMapper.updateScreening(selected);
+    }
+
+    public void deleteScreening(Screening screening){
+        screeningMapper.deleteScreening(screening);
+    }
+
+    public Movie getMovie(int mno){
+        return movieMapper.getMovieForOid(mno);
+    }
+
+    public Screen getScreen(int sno){
+        return screenMapper.getScreenForOid(sno);
+    }
+
+    public Screening getScreenings
     
 }
--- src/main/java/com/application/domain/mappers/MovieMapper.java
+++ src/main/java/com/application/domain/mappers/MovieMapper.java
@@ -102,5 +102,5 @@ public class MovieMapper {
     public getFromCacheByNumber() {
         //TODO
     }
-    
+
 }
