670626da1d1ca6fc0f5084051192729ce3fbedc6
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-12-07 12:21:09 +0800

    add-mapper-impl

---
+++ src/main/java/com/application/db/mappers/ScreeningMapperImpl.java
@@ -0,0 +1,11 @@
+package com.application.db.mappers;
+
+/**
+ * @Author: WangYuyang
+ * @Date: 2021/12/7-12:20
+ * @Project: comp3013j_assignment
+ * @Package: com.application.db.mappers
+ * @Description:
+ **/
+public class ScreeningMapperImpl {
+}
