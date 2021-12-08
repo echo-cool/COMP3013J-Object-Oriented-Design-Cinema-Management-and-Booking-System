0e77276577e1eb6dbe5d9dfd66a11314c9c69382
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-12-08 17:55:48 +0800

    fix-pipeline

---
+++ src/test/java/fxMainTest.java
@@ -0,0 +1,83 @@
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* fxMain Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Dec 8, 2021</pre> 
+* @version 1.0 
+*/ 
+public class fxMainTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: start(Stage primaryStage) 
+* 
+*/ 
+@Test
+public void testStart() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPrimaryStage() 
+* 
+*/ 
+@Test
+public void testGetPrimaryStage() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getRaw() 
+* 
+*/ 
+@Test
+public void testGetRaw() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getUnnamed() 
+* 
+*/ 
+@Test
+public void testGetUnnamed() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getNamed() 
+* 
+*/ 
+@Test
+public void testGetNamed() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
