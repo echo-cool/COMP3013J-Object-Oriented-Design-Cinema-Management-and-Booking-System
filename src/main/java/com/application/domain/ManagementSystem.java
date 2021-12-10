6acbd1ff7a5d263c536aa8d7b5f25d976cda6480
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-12-10 15:34:17 +0800

    update-overlap

--- src/main/java/com/application/domain/ManagementSystem.java
+++ src/main/java/com/application/domain/ManagementSystem.java
@@ -171,23 +171,23 @@ public class ManagementSystem {
         Screening[] screenings = displayScreenings;
         for (Screening screeningDAO : screenings) {
             if (screeningDAO.getScreen().getName().equals(screen_name)) {
-                LocalTime over_time = start_time.plusSeconds(duration);
-                if (over_time.isBefore(screeningDAO.getStartTime()) || start_time.isAfter(screeningDAO.getStartTime().plusSeconds(screeningDAO.getMovie().getDuration()))) {
+                LocalTime over_time = start_time.plusSeconds(duration - 1);
 
-                } else if (over_time.equals(screeningDAO.getStartTime()) || start_time.equals(screeningDAO.getStartTime().plusSeconds(screeningDAO.getMovie().getDuration()))) {
-
-                } else {
+                if ((!over_time.isBefore(screeningDAO.getStartTime())) && (!start_time.isAfter(screeningDAO.getStartTime().plusSeconds(screeningDAO.getMovie().getDuration() - 1)))) {
                     if (selectedScreening != null) {
                         if (screeningDAO.equals(selectedScreening)) {
                             continue;
                         }
                     }
                     return true;
+                } else {
+
                 }
             }
         }
         return false;
     }
+
 
     public void setDate(LocalDate date) {
         this.currentDate = date;
